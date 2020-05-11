66fa7be2e2e2996b79a73b736745e5ea14754aac
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2020-05-11 22:35:46 +0500

    banner (slider)

--- .gulpfile.js
+++ .gulpfile.js
@@ -1,9 +1,10 @@
 		scripts: {
 			src: [
 				'node_modules/jquery/dist/jquery.min.js',
-				base.teplica + '/libs/animate/animate-css.js',
-				base.teplica + '/libs/lazyload.min.js',
-				base.teplica + '/libs/waypoint.js',
+				'node_modules/slick-carousel/slick/slick.min.js',
+				//base.teplica + '/libs/animate/animate-css.js',
+				//base.teplica + '/libs/lazyload.min.js',
+				//base.teplica + '/libs/waypoint.js',
 				base.teplica + '/js/map.js',
 				base.teplica + '/js/common.js', // Custom scripts. Always at the end
 			],
--- js/common.js
+++ js/common.js
@@ -5,4 +5,16 @@ jQuery(function($) {
 		$('.header__menu').toggleClass('active');
 	});
 
+	$('.banner').slick({
+		draggable: true,
+		autoplay: true,
+		arrows: false,
+		dots: false,
+		fade: true,
+		speed: 900,
+		infinite: true,
+		cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+		touchThreshold: 100
+	});
+
 });
